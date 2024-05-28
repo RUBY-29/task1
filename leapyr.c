1838947fd5d76b44bbc490c7df254f0629f97cef
M.Ruby <155981509+RUBY-29@users.noreply.github.com>
2024-05-28 15:01:15 +0530

    Create leapyr.c

---
+++ leapyr.c
@@ -0,0 +1,40 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int year,count=0;
+    printf("enter year:");
+    scanf("%d", &year);
+    
+    while(year!=0)  
+   {  
+       year=year/10;  
+       count++;  
+   } 
+   
+   if(count==4)
+   {
+      if(year%400==0)
+      {
+          printf("this is  leap year \n",year);
+      }
+          else if(year%100==0)
+      {
+          printf("this is not leap year \n",year);
+      }
+      else if(year%4==0)
+      {
+          printf("this is  leap year \n",year);
+      }
+     else
+     {
+         printf("this is not leap year \n",year);
+     }
+   }
+   else
+   {
+       printf("this not considered as year \n",year);
+   }
+   
+    return 0;
+}

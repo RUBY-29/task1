ac56a9d873dd6a4ab8487f789a865f58e1f0439d
M.Ruby <155981509+RUBY-29@users.noreply.github.com>
2024-05-28 13:25:57 +0530

    Create calc.c

---
+++ calc.c
@@ -0,0 +1,36 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+    int num1,num2;
+    char op;
+    printf("enter num1:");
+    scanf("%d", &num1);
+    printf("enter num2:");
+    scanf("%d", &num2);
+    printf("enter op:");
+    scanf(" \n %c", &op);
+    if(op == '+')
+    {
+        printf("res:%d", num1+num2);
+    }
+    else if(op =='-')
+    {
+       printf("res:%d", num1-num2);
+    }
+    else if(op == '*')
+    {
+         printf("res:%d", num1*num2);
+    }
+    else if(op == '/')
+    {
+         printf("res:%d", num1/num2);
+    }
+    else
+    {
+         printf("res:%d", num1%num2);
+    }
+    
+
+    return 0;
+}

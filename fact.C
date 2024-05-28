a82b03ee06d8d1b23a427e0aa8c732b0b1b3c65d
M.Ruby <155981509+RUBY-29@users.noreply.github.com>
2024-05-28 23:00:32 +0530

    Create fact.C

---
+++ fact.C
@@ -0,0 +1,14 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int n,i,fact=1;
+   printf("enter n:");
+   scanf("%d",&n);
+   for(i=1;i<=n;i++)
+   {
+       fact=fact*i;
+   }
+printf("factorial:%d",fact);
+    return 0;
+}

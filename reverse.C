b9026ae077ea2685908e26903594ca6aed7359aa
M.Ruby <155981509+RUBY-29@users.noreply.github.com>
2024-05-29 11:11:42 +0530

    Create reverse.C

---
+++ reverse.C
@@ -0,0 +1,17 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+
+int main() {
+   int i,n,copy,rev;
+   printf("enter n:");
+   scanf("%d",&n);
+   copy=n;
+   while(copy>0)
+   {
+       rev=rev*10;
+       rev=rev+(copy%10);
+       copy=copy/10;
+   }
+   printf("reverse:%d",rev);
+    return 0;
+}
